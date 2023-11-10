595dfe621ea30c0fe37cd36a75b5ec76c60233df
Uziel Cocolan <uziel.atlai.flores@ibm.com>
2023-11-10 00:49:10 -0600

    fix popurri

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import styled from "styled-components"
 import { btn } from './style.module.css'
 
@@ -18,6 +19,8 @@ const Button = styled.button`
 `
 
 export const App = () => {
+  const [isButtonHover, setIsButtonHover] = useState(false)
+
   return <>
     <button className="btn">
       css vanila
@@ -25,11 +28,29 @@ export const App = () => {
     <button className={btn}>
       css module
     </button>
+    <button
+      onMouseEnter={() => setIsButtonHover(true)}
+      onMouseLeave={() => setIsButtonHover(false)}
+      style={{
+        borderRadius: '8px',
+        border: '1px solid transparent',
+        padding: '0.6em 1.2em',
+        fontSize: '1em',
+        fontWeight: 500,
+        fontFamily: 'inherit',
+        backgroundColor: '#1a1a1a',
+        cursor: 'pointer',
+        transition: 'border - color 0.25s',
+        borderColor: isButtonHover ? '#646cff' : 'transparent',
+      }}
+    >
+      style prop
+    </button >
     <Button>
       styled component
     </Button>
     <button
-      className='rounded border border-transparent hover:border-[#646cff] font-medium px-[0.6em] py-[1.2em] bg-[#1a1a1a] transition-[border-color(0.25s)]'
+      className='rounded-lg border border-transparent hover:border-[#646cff] font-medium py-[0.6em] px-[1.2em] bg-[#1a1a1a] transition-[border-color(0.25s)]'
     >
       tailwind
     </button>
