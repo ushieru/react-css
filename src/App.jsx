9d441c7b3fc7474e0816581a8d166d94c0d2d790
Uziel Cocolan <uziel.atlai.flores@ibm.com>
2023-11-10 00:49:37 -0600

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 export const App = () => {
   return <button
-    className='rounded border border-transparent hover:border-[#646cff] font-medium px-[0.6em] py-[1.2em] bg-[#1a1a1a] transition-[border-color(0.25s)]'
+    className='rounded border border-transparent hover:border-[#646cff] font-medium py-[0.6em] px-[1.2em] bg-[#1a1a1a] transition-[border-color(0.25s)]'
   >
     tailwind example
   </button>
