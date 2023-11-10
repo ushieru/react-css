4a7e4336286f20b1538147079af8be62808e9a33
Uziel Cocolan <uziel.atlai.flores@ibm.com>
2023-11-09 23:55:43 -0600

    inline css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import { useState } from "react"
+
 export const App = () => {
-  return <button className='btn'>
+  const [isButtonHover, setIsButtonHover] = useState(false)
+
+  return <button
+    onMouseEnter={() => setIsButtonHover(true)}
+    onMouseLeave={() => setIsButtonHover(false)}
+    style={{
+      borderRadius: '8px',
+      border: '1px solid transparent',
+      padding: '0.6em 1.2em',
+      fontSize: '1em',
+      fontWeight: 500,
+      fontFamily: 'inherit',
+      backgroundColor: '#1a1a1a',
+      cursor: 'pointer',
+      transition: 'border - color 0.25s',
+      borderColor: isButtonHover ? '#646cff' : 'transparent',
+    }}
+  >
     css example
-  </button>
+  </button >
 }
