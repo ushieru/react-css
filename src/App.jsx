00e9070364e38d70e7c14744987d4419f16cc419
Uziel Cocolan <uziel.atlai.flores@ibm.com>
2023-11-10 00:09:51 -0600

    styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import styled from "styled-components"
+
+const Button = styled.button`
+  border-radius: 8px;
+  border: 1px solid transparent;
+  padding: 0.6em 1.2em;
+  font-size: 1em;
+  font-weight: 500;
+  font-family: inherit;
+  background-color: #1a1a1a;
+  cursor: pointer;
+  transition: border-color 0.25s;
+  
+  &:hover {
+    border-color: #646cff;
+  }
+`
+
 export const App = () => {
-  return <button className='btn'>
-    css example
-  </button>
+  return <Button>
+    styled components example
+  </Button>
 }
