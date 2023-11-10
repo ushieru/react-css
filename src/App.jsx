6b71276ad024528f18c21f1b425ebe3887c42ba3
Uziel Cocolan <uziel.atlai.flores@ibm.com>
2023-11-10 00:25:43 -0600

    tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 export const App = () => {
-  return <button className='btn'>
-    css example
+  return <button
+    className='rounded border border-transparent hover:border-[#646cff] font-medium px-[0.6em] py-[1.2em] bg-[#1a1a1a] transition-[border-color(0.25s)]'
+  >
+    tailwind example
   </button>
 }
